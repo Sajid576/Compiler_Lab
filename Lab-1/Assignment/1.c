c10b1cb39aa1c95d12ddb585a9a1aa179fbfe144
Sajid576 <sajidahmed696@gmail.com>
2021-01-05 23:08:08 +0600

    offline_1 modified

--- Lab-1/Assignment/1.c
+++ Lab-1/Assignment/1.c
@@ -1,91 +1,171 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdbool.h>
+
+
+int provideOneSpace=0;
+
+
+void writeOutputFile(char c,FILE *p2)
+{
+         
+        fputc(c, p2);
+
+}
+int checkSlash(FILE *p1)
+{
+    char check = fgetc(p1);
+
+    //single line comment
+    if(check == '/')
+    {
+
+        return 1;
+    }
+    //multi line comment
+    else if(check == '*')
+    {
+
+        return 2;
+    }
+    // other character
+    else
+    {
+        return 3;
+    }
+}
+void removeMultiLineComment(FILE *p1)
+{
+    char c;
+    while((c = fgetc(p1)) != EOF)
+    {
+        if(c == '*')
+        {
+            if((c = fgetc(p1)) == '/')
+            {
+                return;
+            }
+        }
+    }
+}
+void removeSingleLineComment(FILE *p1)
+{
+    char c;
+    while((c = fgetc(p1))!='\n')
+    {
+        continue;
+    }
+}
+void removeExtraSpacesNewlines(FILE *p2)
+{
+    if(provideOneSpace == 0)
+    {
+        writeOutputFile(' ',p2);
+        provideOneSpace = 1;
+    }
+}
 
 
 
+void readInputFile()
+{
+        FILE *p1 = fopen("input.txt","r");
+        char c;
+
+        if(!p1 )
+        {
+            printf("File cannot be opened");
+        }
+        else
+        {
+            while((c = fgetc(p1)) != EOF)
+            {
+                printf("%c",c);
+            }
+        
+        }
+        fclose(p1);
+}
+void readOutputFile()
+{
+        FILE *p1 = fopen("output.txt","r");
+        char c;
+
+        if(!p1 )
+        {
+            printf("File cannot be opened");
+        }
+        else
+        {
+            while((c = fgetc(p1)) != EOF)
+            {
+                printf("%c",c);
+            }
+        
+        }
+        fclose(p1);
+}
 
 int main(void)
 {
-    char str[10000];
-    char original[10000];
-    FILE *p1,*p2;
+    FILE *p1, *p2;
     char c;
-    p1=fopen("sample.txt","r");
-    p2=fopen("sample.txt","r");
-   
+
+    p1 = fopen("input.txt","r");
+    p2 = fopen("output.txt", "w");
 
     if(!p1 || !p2)
     {
-        printf("File can't be opened");
+        printf("File cannot be opened");
     }
     else
     {
-        int i=0;
-        int j=0;
-         while((c=fgetc(p2))!=EOF)
-         {
-             original[j++]=c;
-         }
-         printf("original file is: \n");
-         puts(original);
-
-        int checkSpaceON=0;
-        while((c=fgetc(p1))!=EOF)
+        
+        while((c = fgetc(p1)) != EOF)
         {
-            
+
             //printf("%c",c);
-            if(c=='\n'|| c=='\r' || c=='\t' || c==' ')
+            if(c != ' ' && c!='\n')
             {
-                continue;
-            }
-            
-            if(c=='/' )
-            {
-                
-                c=fgetc(p1);
-                if(c=='/')
+
+                if(c == '/')
                 {
-                    //single line comment detected...
-                     while((c=fgetc(p1))!='\n')
-                     {
-                            continue;
-                     }
-                }
-                else if(c=='*')
-                {
-                    //multi line comment detected...
-                    while(1)
+
+                    int check = checkSlash(p1);
+                    if(check == 1)
                     {
-                        c=fgetc(p1);
-                        if(c=='*')
-                        {
-                            c=fgetc(p1);
-                            if(c=='/')
-                            {
-                                break;
-                            }
-                        }
-                        else 
-                        {
-                            continue;
-                        }
-                         
+                        removeSingleLineComment(p1);
+                    }
+                    else if(check == 2)
+                    {
+                        removeMultiLineComment(p1);
+                    }
+                    else
+                    {
+                        provideOneSpace = 0;
+                        writeOutputFile(c,p2);
                     }
                 }
-                
-
+                else
+                {
+                    provideOneSpace = 0;
+                    writeOutputFile(c,p2);
+                }
             }
-            else 
+            else if(c == ' ' || c =='\n')
             {
-
-                str[i++]=c;
+                removeExtraSpacesNewlines(p2);
             }
-           
-            
         }
-        printf("Compressed file is: \n");
-        puts(str);
-        
     }
+
+    fclose(p1);
+    fclose(p2);
+    readInputFile();
+    readOutputFile();
+
+   
+
+
 }
