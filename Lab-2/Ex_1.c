60c7542250c66b9db17bc5e2040d0639fd7709f0
Sajid576 <sajidahmed696@gmail.com>
2021-01-11 15:59:08 +0600

    Lab_2 exercises added

---
+++ Lab-2/Ex_1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+int main(void)
+{
+char keyword[32][100]={
+   "switch","case","enum","register","typedef","char",
+    "goto","sizeof","voltile","do","if","static","while"
+      "auto","double","int","struct","break","else","long",
+       "extern","return","union","const","float","short",
+      "unsigned","continue","for","signed","void","default",
+     
+   } ;
+   char str[]="double";
+   int flag=0;
+   int i;
+  
+   for( i = 0; i < 32; i++) {
+      if(strcmp(str,keyword[i])==0) {
+         flag=1;
+      }
+   }
+   if(flag==1)
+      printf("%s is a keyword",str);
+   else
+      printf("%s is not a keyword",str);
+}
---
+++ Lab-2/Ex_2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+bool isValid(char *str, int n) 
+{ 
+    int i;
+     
+    if (!((str[0] >= 'a' && str[0] <= 'z') 
+          || (str[0] >= 'A' && str[1] <= 'Z') 
+          || str[0] == '_')) 
+        return false; 
+  
+    
+    for ( i = 1; i < strlen(str); i++) { 
+        if (!((str[i] >= 'a' && str[i] <= 'z') 
+              || (str[i] >= 'A' && str[i] <= 'Z') 
+              || (str[i] >= '0' && str[i] <= '9') 
+              || str[i] == '_')) 
+            return false; 
+    } 
+  
+     
+    return true; 
+} 
+
+int main(void)
+{
+    char str[] = "_lol"; 
+    int n = strlen(str); 
+  
+    if (isValid(str, n)) 
+        printf( "Valid"); 
+    else
+       printf( "Invalid"); 
+
+}
